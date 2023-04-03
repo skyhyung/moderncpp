1fcc040a2b7d8f9d1a6745817c0824dbbcb0b675
ecourse <smkang@codenuri.co.kr>
2023-04-03 14:45:14 +0900

    Update 18_if_init.cpp

--- DAY1/18_if_init.cpp
+++ DAY1/18_if_init.cpp
@@ -9,4 +9,28 @@ int main()
 	{
 	}
 
+	// C++17 부터 init if 도입됩니다.
+	if (int ret2 = foo(); ret2 == 0)
+	{
+		// ret2 사용
+	} //<== 여기서 ret2 파괴
+
+	switch (int n = foo(); n)
+	{
+	case 1:break;
+	}
+
+	// while 도 될까요 ? => 안됩니다. for 문으로 하면 되기 때문에..!
+	/*
+	while (int cnt = 0; cnt < 10)
+	{
+		++cnt;
+	}
+	*/
+
 }
+
+
+
+
+
