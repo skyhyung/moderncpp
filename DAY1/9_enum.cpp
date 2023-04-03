ef9a1544e0e0179960f0091614e610df57a16587
ecourse <smkang@codenuri.co.kr>
2023-04-03 13:36:36 +0900

    Update 9_enum.cpp

--- DAY1/9_enum.cpp
+++ DAY1/9_enum.cpp
@@ -1,10 +1,19 @@
 // 9_enum - 32 page
+enum DAYOFWEEK { SUN = 0, MON = 1, TUE = 2};
 
-void foo(int dayofaweek) {}
+//void foo(int dayofaweek)     {} // bad
+void foo(DAYOFWEEK dayofaweek) {} // good
 
 int main()
 {
-	foo(0);  
+//	foo(0);  
+	foo(SUN);	// ok
+//	foo(10);	// error
+
+	int MON = 10; // ok
+//	foo(MON); // error. 지역변수
+	foo(SUN); // ok. 하지만 아래 코드가 더 좋다.
+	foo(DAYOFWEEK::SUN); // good
 }
 
 
