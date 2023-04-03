eab4c40781eb06b9be07db468b14f7bdfeb08b89
ecourse <smkang@codenuri.co.kr>
2023-04-03 16:36:51 +0900

    Update 20_타입추론3.cpp

--- DAY1/20_타입추론3.cpp
+++ DAY1/20_타입추론3.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+// 규칙 2. 함수 인자가 reference 일때 (T& a)
+// => 함수 인자가 가진 "reference" 속성만 제거 하고 T를 결정
+// => const, volatile 속성은 유지
 template<typename T> void foo(T& a)
 {
-	a = 100;
+	std::cout << __FUNCSIG__ << std::endl;
 }
 
 int main()
@@ -12,8 +14,8 @@ int main()
 	const int c = 10;
 	const int& cr = c;
 
-	foo(n);  
-	foo(c);  
-	foo(r);  
-	foo(cr); 
+	foo(n);  // T=int			a=int&
+	foo(c);  // T=const int		a=const int&
+	foo(r);  // T=int			a=int&
+	foo(cr); // T=const int		a=const int&
 }
