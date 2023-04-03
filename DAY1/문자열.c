68913907d0fd9e9cb981cc7e7da47a14ca7238d6
ecourse <smkang@codenuri.co.kr>
2023-04-03 17:12:21 +0900

    Update 문자열.c

--- DAY1/문자열.c
+++ DAY1/문자열.c
@@ -6,10 +6,22 @@
 int main()
 {
 	char s1[] = "ABCD";
-	char* s2 = "ABCD";
+//	char* s2 = "ABCD"; // C 언어 : ok.   C++ : 에러
 
-	*s1 = 'X';
-	*s2 = 'X';
+	const char* s2 = "ABCD"; // C++ 은 이렇게만 가능.
+							// *s2 = 'X' 가 런타임 에러가 아닌
+							// 컴파일 시간 에러!
+
+	*s1 = 'X';	// ok. stack 이므로 R/W 가능
+	*s2 = 'X';  // C언어 : runtime error. 상수메모리를 Write 시도
 
 	printf("main continue\n");
+
+	//------------------
+	const char* s3 = "hello";
+	const char* s4 = "hello";
+
+	// 아래 2개의 주소를 생각해 봅시다.
+	printf("%p\n", s3);
+	printf("%p\n", s4);
 }
