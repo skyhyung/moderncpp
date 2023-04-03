79d6c374ae59ebcc0ce1d84de9e025d1792d2775
ecourse <smkang@codenuri.co.kr>
2023-04-03 09:30:35 +0900

    Update 1_static_assert1.cpp

--- DAY1/1_static_assert1.cpp
+++ DAY1/1_static_assert1.cpp
@@ -5,6 +5,7 @@
 // => 컴파일 시간 assert
 // => 함수 안 또는 밖에 있어도 됩니다.
 // => 표현식은 반드시 컴파일 시간에 조사 가능해야 합니다.
+//    static_assert(age > 0, "error"); // 이 표현식은 안됨!
 
 static_assert(sizeof(int*) >= 8, "error not 64bit");
 
@@ -14,6 +15,7 @@ void foo(int age)
 	assert(age > 0); // 실행시간 표현식의 유효성 조사
 
 //	static_assert(sizeof(int*) >= 8, "error not 64bit");
+
 
 
 	int* p = new int[age];
