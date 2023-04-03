aad4ed49c522ca2a485d1e361e9faf18a02a8164
ecourse <smkang@codenuri.co.kr>
2023-04-03 14:01:15 +0900

    Update 10_user_define_literal2.cpp

--- DAY1/10_user_define_literal2.cpp
+++ DAY1/10_user_define_literal2.cpp
@@ -5,14 +5,25 @@
 #include <thread>
 using namespace std::chrono; 
 
-void foo(const char* s) { std::cout << "char*"; }
-void foo(std::string s) { std::cout << "string"; }
+using namespace std::literals; // C++ 표준이 제공하는 리터럴 접미사!
+
+void foo(const char* s) { std::cout << "char*"; }	// 1	
+void foo(std::string s) { std::cout << "string"; }  // 2
 
 int main()
 {
-	foo("hello");
-	foo("hello");
-				
+	foo("hello");  // 1
+	foo("hello"s); // 2.  std::string operator""s("hello")
+	
+//	std::this_thread::sleep_for( 3 ); // error
+
+	std::this_thread::sleep_for( std::chrono::seconds(3) ); // ok
+	std::this_thread::sleep_for( 3s ); // 위와 동일
+
+	std::chrono::seconds s1 = 1h + 3min + 3s;
+
+	std::cout << s1.count() << std::endl; // 3783
+
 }
 
 
