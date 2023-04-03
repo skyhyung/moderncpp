5410da74fd4a57a494c47418416a24ba2898e835
ecourse <smkang@codenuri.co.kr>
2023-04-03 13:42:05 +0900

    Update 9_enum2.cpp

--- DAY1/9_enum2.cpp
+++ DAY1/9_enum2.cpp
@@ -1,18 +1,21 @@
-enum DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+//enum DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+
+// C++11 의 새로운 enum : scoped enum 이라고 합니다.
+enum class DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
 
 //void foo(int dayofaweek)     {} // bad
 void foo(DAYOFWEEK dayofaweek) {} // good
 
 int main()
 {
-
-	int n1 = DAYOFWEEK::MON;
-	DAYOFWEEK d1 = 10;
-
-	DAYOFWEEK d2 = MON;
-	DAYOFWEEK d3 = DAYOFWEEK::MON;
+	//							C/C++98		C++11
+	int n1 = DAYOFWEEK::MON; // ok			error
+	DAYOFWEEK d1 = 10;		 // error		error
+	DAYOFWEEK d2 = MON;		// ok.			error
+	DAYOFWEEK d3 = DAYOFWEEK::MON; // ok
 
 }
+
 
 
 
