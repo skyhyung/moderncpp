241f80cb6045d343278f916336f5ce08892869cd
ecourse <smkang@codenuri.co.kr>
2023-04-03 17:02:14 +0900

    Update 20_타입추론5-1.cpp

--- DAY1/20_타입추론5-1.cpp
+++ DAY1/20_타입추론5-1.cpp
@@ -1,12 +1,17 @@
 // 20_타입추론5-1
-template<typename T>
-void foo(T a, T b) { }
-
-template<typename T>
-void goo(T& a, T& b) { }
-
+template<typename T> void foo(T a,  T b) { }
+template<typename T> void goo(T& a, T& b) { }
 int main()
 {
-	foo("apple", "banana");
+	// "apple"  타입 : const char[6]
+	// "banana" 타입 : const char[7]
+	
 	goo("apple", "banana"); // error?? 왜 ?
+						// 참조로 받으면 배열=> 배열로 전달
+						// 같은 타입을 요구하는 데, "apple" 과 banaba는
+						// 다른 타입(크기가 다르므로)
+
+	foo("apple", "banana"); // 값타입이므로
+							// const char[6] => const char* 로전달
+							// const char[7] => const char* 로전달
 }
