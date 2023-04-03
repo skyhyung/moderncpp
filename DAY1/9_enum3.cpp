aff66f6f6795cd7fd83f53f030ad0ba9498eed7a
ecourse <smkang@codenuri.co.kr>
2023-04-03 13:46:11 +0900

    Update 9_enum3.cpp

--- DAY1/9_enum3.cpp
+++ DAY1/9_enum3.cpp
@@ -1,8 +1,15 @@
+#include <type_traits>
+#include <iostream>
 
-enum class DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+// C++98 enum.. 요소의 타입은 int 입니다. 변경할수 없습니다.
+// enum  DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 }; 
+
+enum class DAYOFWEEK : char { SUN = 0, MON = 1, TUE = 2 };
 
 int main()
 {
+	std::cout << typeid(std::underlying_type_t<DAYOFWEEK>).name()
+		      << std::endl;
 }
 
 
