9ec467e4103cd3cc0e131a255591e86fb9f89bbd
ecourse <smkang@codenuri.co.kr>
2023-04-03 10:34:36 +0900

    Update 4_beginend.cpp

--- DAY1/4_beginend.cpp
+++ DAY1/4_beginend.cpp
@@ -5,10 +5,36 @@
 
 int main()
 {
-	std::vector<int> v = { 1,2,3,4,5 };
+	//std::vector<int> v = { 1,2,3,4,5 };
 	//std::list<int>   v = { 1,2,3,4,5 };
-	//int v[5] = { 1,2,3,4,5 };
+	int v[5] = { 1,2,3,4,5 };
 
-	std::vector<int>::iterator p1 = v.begin();
-	std::vector<int>::iterator p2 = v.end();
+	// C++98 스타일의 반복자 꺼내기
+	// 반복자 타입이름을 직접 사용하면
+	// 1. 복잡하다.
+	// 2. 컨테이너 변경시 반복자 타입 이름도 변경해야 한다.
+//	std::vector<int>::iterator p1 = v.begin();
+//	std::vector<int>::iterator p2 = v.end();
+
+	// C++11 부터는 auto 가 좋습니다.
+//	auto p1 = v.begin();
+//	auto p2 = v.end();
+
+	// 멤버 함수 begin 보다 일반 함수 begin 이 유연성이 좋다.
+	auto p1 = std::begin(v);
+	auto p2 = std::end(v);   // 배열도 가능.
+
+//	auto sz1 = v.size();	 // v가 배열이면 에러
+	auto sz1 = std::size(v); // v가 배열이라도 ok.
+
+
+	// C++20 스타일
+	auto p3 = std::range::begin(v); // <== C++20 부터는 강력권장(안전)
+
+	// 기존 begin 의 문제점
+	auto p4 = std::begin( std::vector<int>{1, 2, 3} ); // ok
 }
+
+
+
+
