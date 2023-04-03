cd5ddd98f2c81d8ffa269ce0150b9f239c73bf21
ecourse <smkang@codenuri.co.kr>
2023-04-03 14:10:09 +0900

    Update 13_default_function.cpp

--- DAY1/13_default_function.cpp
+++ DAY1/13_default_function.cpp
@@ -7,6 +7,11 @@ class Point
 	int x, y;
 public:
 	Point(int a, int b) {}
+
+	// 디폴트 생성자를 만드는 2가지 방법
+//	Point() {}	// 방법 1. 사용자가 직접 만든다.
+
+	Point() = default; // 방법 2. 컴파일러에게 만들어 달라고 요청
 };
 
 int main()
