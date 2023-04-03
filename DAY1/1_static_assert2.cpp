dedf4fd5068e77c484276cb8913199625c156842
ecourse <smkang@codenuri.co.kr>
2023-04-03 09:39:10 +0900

    Update 1_static_assert2.cpp

--- DAY1/1_static_assert2.cpp
+++ DAY1/1_static_assert2.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <type_traits> 
-
+//#pragma pack(1)		// 멤버 align 을 1byte 단위로!
 struct PACKET
 {
 	char cmd;
 	int  data;
-};
+}; 
 
-int main()
-{
-	std::cout << sizeof(PACKET) << std::endl; // ?
-}
+static_assert(sizeof(PACKET) == sizeof(char) + sizeof(int),
+				"error. unexpected padding");
 
 template<typename T> void object_set_zero(T* p)
 {
+	static_assert(!std::is_polymorphic<T>::value,
+				"error T has virtual function");
+
 	memset(p, 0, sizeof(T)); 
 }
+class A
+{
+	int n;
+public:
+	virtual void foo() {}
+};
+int main()
+{
+	A a;
+	object_set_zero(&a);
+	std::cout << sizeof(PACKET) << std::endl; // ?
+}
+
+
