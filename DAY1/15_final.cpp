d50be0ea16198b47d7ea43970c891c87e308c8ae
ecourse <smkang@codenuri.co.kr>
2023-04-03 14:40:44 +0900

    Update 15_final.cpp

--- DAY1/15_final.cpp
+++ DAY1/15_final.cpp
@@ -9,7 +9,15 @@ public:
 class Derived : public Base
 {
 public:
-	virtual void foo()      override {} 
+	virtual void foo()      override final {} // 더이상 재정의 안됨
+	virtual void goo(int a) override {}
+	virtual void hoo()      override {}
+};
+
+class Derived2 : public Derived
+{
+public:
+	virtual void foo()      override {} // error
 	virtual void goo(int a) override {}
 	virtual void hoo()      override {}
 };
