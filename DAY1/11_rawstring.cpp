3d4c3c4c21f2c5e6d35ecb9cc4d1eef94cbd9750
ecourse <smkang@codenuri.co.kr>
2023-04-03 12:13:41 +0900

    Update 11_rawstring.cpp

--- DAY1/11_rawstring.cpp
+++ DAY1/11_rawstring.cpp
@@ -5,4 +5,20 @@
 int main()
 {
 	std::string s1 = "\\\\.\\pipe\\server";
+
+	// raw string : \ 를 일반 문자로 취급
+	// 문자열 시작 : "(
+	//        종료 : )"
+//	std::string s2 = R"(\\.\pipe\server)";
+
+	// " 표기하려면
+//	std::string s2 = R"(\\.\pi"pe\server)";
+
+
+	// 핵심 : )" 를 표기하려면
+	std::string s2 = R"***(\\.\pi)"pe\server)***";
+
+
+	std::cout << s1 << std::endl;
+	std::cout << s2 << std::endl;
 }
