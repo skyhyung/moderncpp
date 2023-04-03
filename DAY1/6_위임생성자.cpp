4e0282e40570ae48b419c1e8c2e597be0a1a81b9
ecourse <smkang@codenuri.co.kr>
2023-04-03 11:08:28 +0900

    Update 6_위임생성자.cpp

--- DAY1/6_위임생성자.cpp
+++ DAY1/6_위임생성자.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-// 6_���ӻ����� - 24page
+// 6_위임생성자 - 24page
 class Point
 {
 public:
 	int x, y;
 public:
-	Point() 
+	Point() : Point(0,0)   // C++11 부터 다른 생성자 호출가능
+							// "위임 생성자"라는 문법
 	{
+//		Point(0, 0); // 다른 생성자 호출이 아니라!!
+					 // 이순간 이름없는 객체(임시객체)를 만들게 됩니다
+					// C++11 이전까지는 생성자에서 다른 생성자 호출 안됩니다.
 	}
 	Point(int a, int b) : x(a), y(b)
 	{
-		// ������ �ʱ�ȭ �ڵ�
+		// 복잡한 초기화 코드
 	}
 };
 int main()
