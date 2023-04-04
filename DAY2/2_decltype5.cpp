df66868a3e94ef4b7d50949bba66e6aa00600ce9
ecourse <smkang@codenuri.co.kr>
2023-04-04 09:17:43 +0900

    Update 2_decltype5.cpp

--- DAY2/2_decltype5.cpp
+++ DAY2/2_decltype5.cpp
@@ -1,11 +1,16 @@
-int x = 10;
-int& foo() { return x;  }
-
 int main()
 {
 	int x[3] = { 1,2,3 };
 
-	auto a = x[0];
-	decltype(x[0]) d; 
+	
+	auto a = x[0];		// int a = x[0];
+
+	decltype(x[0]) d;   // int& d;   초기값 문제로 erro
+
+
+	// 배열과 타입 추론
+	auto a1 = x;		// int* a1 = x
+
+	decltype(x) d1;		// int d1[3];
 }
 
