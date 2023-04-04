de83519b5f3a5b4145f16f2b39616148ff774568
ecourse <smkang@codenuri.co.kr>
2023-04-04 15:10:36 +0900

    Update 7_move04.cpp

--- DAY2/7_move04.cpp
+++ DAY2/7_move04.cpp
@@ -1,1 +1,33 @@
-// 03¹ø º¹»ç ÇØ¿À¼¼¿ä..
+#include <iostream>
+
+class Cat
+{
+	char* name;
+	int   age;
+public:
+	Cat(const char* n, int a) : age(a)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+	}
+	~Cat() { delete[] name; }
+
+	Cat(const Cat& c) : age(c.age)
+	{
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+	}
+};
+Cat foo()
+{
+	Cat c("yaong", 3);
+	return c;
+}
+int main()
+{
+	Cat c1("nabi", 2);
+	Cat c2 = c1;
+
+	Cat c3 = c; // ì´ìˆœê°„ ë©”ëª¨ë¦¬ ê·¸ë¦¼ì„ ìƒê°í•´ ë³´ì„¸ìš”
+				// 
+}
