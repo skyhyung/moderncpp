0f6ea0ea27c1cb20c56db74a9b3d117fdddbb95a
ecourse <smkang@codenuri.co.kr>
2023-04-04 16:03:58 +0900

    Update 7_move07.cpp

--- DAY2/7_move07.cpp
+++ DAY2/7_move07.cpp
@@ -1,1 +1,79 @@
-// 6¹ø º¹»ç. move °è¿­ÇÔ¼ö ¿¡¼­´Â ¸ğµç ¸â¹ö¸¦ move·Î ¿Å°Ü¶ó!
+// 6ë²ˆ ë³µì‚¬. move ê³„ì—´í•¨ìˆ˜ ì—ì„œëŠ” ëª¨ë“  ë©¤ë²„ë¥¼ moveë¡œ ì˜®ê²¨ë¼!
+#include <iostream>
+#include <string>
+
+class Cat
+{
+	char* name;
+	int   age;
+	std::string address;
+public:
+	Cat(const char* n, int a, const std::string& s = "unknown")
+		: age(a), address(s)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+	}
+	~Cat() { delete[] name; }
+
+	Cat(const Cat& c) : age(c.age), address(c.address)
+	{
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+		std::cout << "ë³µì‚¬" << std::endl;
+	}
+
+	Cat(Cat&& c)
+		//: age(c.age), name(c.name), address(c.address)
+		: age(c.age), name(c.name), address(std::move(c.address))
+	{
+		c.name = nullptr;
+		std::cout << "ì´ë™" << std::endl;
+	}
+	Cat& operator=(const Cat& c)
+	{
+		if (&c == this) return *this;
+
+		address = c.address;
+		age = c.age;
+		delete[] name;
+		name = new char[strlen(c.name) + 1];
+		strcpy_s(name, strlen(c.name) + 1, c.name);
+
+		return *this;
+	}
+
+	Cat& operator=(Cat&& c)
+	{
+		if (&c == this) return *this;
+
+		address = std::move(c.address);
+
+		age = c.age;
+		delete[] name;
+
+		name = c.name;
+		c.name = nullptr;
+
+		return *this;
+	}
+};
+
+
+
+
+int main()
+{
+	Cat c1("nabi", 2);
+	Cat c2 = c1;	
+	c2 = c1;		
+
+	Cat c3 = std::move(c1);
+	c3 = std::move(c2);
+
+}
+
+
+
+
+
