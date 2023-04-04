c6715662cd2832d7177adbc2b30b3232796823e0
ecourse <smkang@codenuri.co.kr>
2023-04-05 08:57:36 +0900

    Update 상수멤버함수2.cpp

--- DAY3/상수멤버함수2.cpp
+++ DAY3/상수멤버함수2.cpp
@@ -1,16 +1,30 @@
-
 class Point
 {
 public:
 	int x, y;
-	void set(int a, int b) { x = a; y = b; }
+	void set(int a, int b) // void set(Point* this, int a, int b)
+	{ 
+		x = a;		// this->x = a;
+		y = b;		// this->y = b;
+	}
+	/*
+	// C++23 부터 멤버 함수 작성시 this를 직접 표기하는 문법등장
+	void set2(this Point& pt, int a, int b)
+	{
+		pt.x = a;
+		pt.y = b;
+	}
+	*/
 };
-
 int main()
 {
 	Point p1;
 	Point p2;
 
-	p1.set(10, 20);
-	p2.set(10, 20);
+	p1.set(10, 20); // set(&p1, 10, 20)
+	p2.set(10, 20); // set(&p2, 10, 20)
+
+	const Point p3;
+	p3.set(10, 20); // set(&p3, 10, 20);
+					// 인데, p3가 const 객체 이므로error
 }
