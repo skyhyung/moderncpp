951ca454dbc54751264ce35594c7747892b9e200
ecourse <smkang@codenuri.co.kr>
2023-04-04 10:04:35 +0900

    Update 3_초기화3-1.cpp

--- DAY2/3_초기화3-1.cpp
+++ DAY2/3_초기화3-1.cpp
@@ -6,11 +6,16 @@ class Point
 public:
 	Point(int a, int b) : x(a), y(b) {}
 };
-void f1(Point pt) {}
-void f2(int n) {}
+void f1(Point pt) {} // Point pt = {1,2}
+void f2(int n) {} // int n = {3}
+
+Point f3() { return { 1,2 }; }
 
 int main()
 {
-	int n = 3;
-	f2(3);
+//	int n = 3;
+	int n = { 3 };
+	f2({ 3 });
+
+	f1({ 1,2 });
 }
