713981ae43eb9646b8fc64e6cdc08734d630b7bc
ecourse <smkang@codenuri.co.kr>
2023-04-04 09:46:05 +0900

    Update 3_초기화1-1.cpp

--- DAY2/3_초기화1-1.cpp
+++ DAY2/3_초기화1-1.cpp
@@ -2,6 +2,7 @@
 
 class Point
 {
+	// C++11. field initialization
 	int x = 0;
 	int y = 0;
 public:
@@ -9,7 +10,19 @@ public:
 	Point(int b) : y(b) {}
 };
 
+// 위 코드를 컴파일러는 아래 처럼 변경합니다.
+class Point
+{	
+	int x; // = 0;
+	int y; // = 0;
+public:
+	// 위 필드 초기값을 컴파일러가 생성자의 초기화 리스트에 추가
+	Point()      : x(0), y(0) {}
+	Point(int b) : x(0), y(b) {}
+};
 int main()
 {
 
 }
+
+
