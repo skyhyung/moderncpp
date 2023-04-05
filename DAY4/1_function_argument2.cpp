a33c7eb69289357481f12a205a317f651a7d43f6
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-06 08:48:28 +0900

    Update 1_function_argument2.cpp

--- DAY4/1_function_argument2.cpp
+++ DAY4/1_function_argument2.cpp
@@ -5,6 +5,8 @@ int main()
 {
 	std::string s = "to be or not to be";
 
-	std::string s1 = s;
-	std::string_view sv = s;  
+	std::string s1 = s; 	   // s의 문자열을 s1으로 복사. string은 문자열을 소유한다.
+	std::string_view sv = s;   // sv는 s가 가지고 있는 문자열을 보기만한다. 
+				   // 문자열을 소유하지 않는다. (자원을 소유하지 않는다)
+				   // 그러므로 크기가 작다.
 }
