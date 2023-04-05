0099c195a2252a7b9e1c85c1e77a4f611a9231d8
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-06 08:51:21 +0900

    Update 1_functon_argument.cpp

--- DAY4/1_functon_argument.cpp
+++ DAY4/1_functon_argument.cpp
@@ -10,8 +10,9 @@ void f2(const std::string& s) { } // good
 void f3(std::string_view s)   { } // best, C++17 부터
 
 // 아래 코드는 어떨까요 ?
-void f3(int n)		  { int a = n; } // good
-void f4(const int& n) { int a = n; } // bad
+void f3(int n)		  { int a = n; } // good. 어차피 int 복사는 참조와 큰 차이가 없다.
+void f4(const int& n) { int a = n; } 	// bad. 간접 참조이기에 역참조를 하기 위한 오버헤드가 발생한다.
+					// godbolt 에서 확인하니 asm code 한줄 증가.
 
 // 핵심. in parameter 를 만들때(인자를 읽기만 하는 경우)
 // primitive type   : call by value
