fddf5bba8a17f81c06cf653e94b9369a7606933a
ecourse <smkang@codenuri.co.kr>
2023-04-05 16:52:39 +0900

    Update 6_함수객체6.cpp

--- DAY3/6_함수객체6.cpp
+++ DAY3/6_함수객체6.cpp
@@ -17,15 +17,27 @@ struct Greater
 {
 	inline bool operator()(int a, int b) { return a > b; }
 };
-
+//-----------------------------------------------
+// 비교 정책을 교체할수 있는데, 비교 정책 자체가 인라인 치환도 된다.
+// => 템플릿과 함수 객체를 사용하는 기술
+// => STL 설계의 핵심 원리 
+template<typename T>
+void Sort(int* x, int sz, T cmp )
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = i + 1; j < sz; j++)
+		{
+			if (cmp(x[i], x[j]) == true)
+				std::swap(x[i], x[j]);
+		}
+	}
+}
 
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
 
-	Less    f1; f1(1, 2); 
-	Greater f2; f2(1, 2); 
+	Less    f1; f1(1, 2); Sort(x, 10, f1); // ok
+	Greater f2; f2(1, 2); Sort(x, 10, f2); // ?
 }
-
-
-
