3707d23a7a12da79091ed446a46f7da182de5aca
ecourse <smkang@codenuri.co.kr>
2023-04-05 12:11:38 +0900

    Update 3_완벽한전달활용2.cpp

--- DAY3/3_완벽한전달활용2.cpp
+++ DAY3/3_완벽한전달활용2.cpp
@@ -22,7 +22,14 @@ int main()
 	// 스마트 포인터
 	// 1. 직접 객체 생성
 	// 아래 코드는 동적 메모리를 몇번 할당 할까요 ?
-	std::shared_ptr<Point> sp1(new Point(1,2));
+	//std::shared_ptr<Point> sp1(new Point(1,2));
+
+	// 스마트 포인터 사용시 std::make_shared<> 를 사용하는것이
+	// 좋습니다.
+	std::shared_ptr<Point> sp1 = std::make_shared<Point>(1, 2);
+				// 1. sizeof(Point) + sizeof(관리객체)를 한번에 할당
+				// 2. 인자로 전달받은 1,2 를 가지고 Point 생성자 호출
+				// 3. 생성된 객체를 관리하는 shared_ptr을 만들어서 반환
 }
 
 
