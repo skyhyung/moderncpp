9cf382b92087a09796f6dce4bed55877fdf0b1a0
ecourse <smkang@codenuri.co.kr>
2023-04-05 17:05:06 +0900

    Update 6_함수객체7.cpp

--- DAY3/6_함수객체7.cpp
+++ DAY3/6_함수객체7.cpp
@@ -23,12 +23,19 @@ int main()
 	std::sort(x, x + 10, cmp1); // sort(int*, int*, bool(*)(int,int))인 함수 생성
 	std::sort(x, x + 10, cmp2); // sort(int*, int*, bool(*)(int,int))인 함수 생성
 
+
 	// 2. 비교 정책으로 함수객체 사용시
+	// 장점 : 비교정책이 인라인 치환된다. 빠르다!
+	
+	// 단점 : 정책을 교체한 횟수 만큼의 sort() 함수 생성
+	//		 코드 메모리 증가
+
 	Less    f1;
 	Greater f2;
-	std::sort(x, x + 10, f1); // sort(?) 모양의 함수 생성
-	std::sort(x, x + 10, f2); // sort(?) 모양의 함수 생성
+	std::sort(x, x + 10, f1); // sort(int*, int*, Less) 모양의 함수 생성
+	std::sort(x, x + 10, f2); // sort(int*, int*, Greater) 모양의 함수 생성
 }
+
 
 
 
