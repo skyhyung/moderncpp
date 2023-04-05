42969e9248be7e400b1f0813989fbeb2268e9063
ecourse <smkang@codenuri.co.kr>
2023-04-05 16:05:49 +0900

    Create 6_함수객체2-1.cpp

---
+++ DAY3/6_함수객체2-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bitset>	// 비트단위로 관리하는 STL
+
+class URandom
+{
+	std::bitset<10> bs;
+	bool recycle;
+public:
+	URandom(bool b = false) : recycle(b)
+	{
+//		bs.set(3); // 3번째 비트만 1로
+		bs.set();	// 모든 비트를 1로
+	}
+	int operator()()
+	{
+		int k = -1;
+
+		while (!bs.test(k = rand() % 10) );
+
+		bs.reset(k);
+
+		return k;
+	}
+};
+URandom urand;
+
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+		std::cout << urand() << std::endl;
+}
