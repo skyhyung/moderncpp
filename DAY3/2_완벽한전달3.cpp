953ad900d065593455b09a1ed6896adb40d665ed
ecourse <smkang@codenuri.co.kr>
2023-04-05 10:54:35 +0900

    Update 2_완벽한전달3.cpp

--- DAY3/2_완벽한전달3.cpp
+++ DAY3/2_완벽한전달3.cpp
@@ -6,27 +6,28 @@ void goo(int& a) { a = 100; }
 void hoo(int&& a) {}
 
 
-// 핵심 : int& 와 int&& 버전을 2개 제공해야 합니다.
 template<typename F> void chronometry(F f, int& arg)
 {
 	f(arg);
 }
 
+// main						chronometry				hoo(int&&)
+// hoo(10) ============================================> ok
+// chronometry(10) ========> int&& arg = 10
+//							hoo(arg) ==================> error(arg는 lvalue)
+//							hoo(static_cast<int&&>(arg)) ==> ok
+
 template<typename F> void chronometry(F f, int&& arg)
 {
-	f(arg);
+	// 인자를 받을때 "int&& arg = 10" 이 된다.
+	// => 상수성도 추가되지 않고, 복사본도 없지만
+	// => rvalue 인 10을 받으면서 arg라는 이름이 생겨서 lvalue
+	//	  로 변경
+//	f(arg); // hoo(int&) 를 찾게 되므로 error
+	f(static_cast<int&&>(arg)); // ok!!!
 }
 
 int main()
 {
-	int n = 10;
-
-	chronometry(foo, 10); 
-	chronometry(goo, n);  
-
-	hoo(10); // ok
-	chronometry(hoo, 10);	//error.. 왜???
-							// 해결은 다음 소스
-
-	std::cout << n << std::endl;
+	chronometry(hoo, 10);	
 }
