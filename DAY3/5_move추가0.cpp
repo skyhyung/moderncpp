d91f25ea6ead593f8e81a0c8fcf3997fc49c9848
ecourse <smkang@codenuri.co.kr>
2023-04-05 14:40:31 +0900

    Update 5_move추가0.cpp

--- DAY3/5_move추가0.cpp
+++ DAY3/5_move추가0.cpp
@@ -6,13 +6,22 @@ class Object
 {
 public:
 	Object() {}
-	Object(const Object& o) { std::cout << "copy" << std::endl; }
-	Object(Object&& o)      { std::cout << "move" << std::endl; }
+	Object(const Object& o)     { std::cout << "copy" << std::endl; }
+	Object(Object&& o) noexcept { std::cout << "move" << std::endl; }
 };
+
+// C++ 표준의 move 를 직접 구현해 봅시다.
+template<typename T>
+T&& move(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
 
 int main()
 {
 	Object o1;
-	Object o2 = o1;			   // copy
-	Object o3 = std::move(o1); // move
+	Object o2 = o1;  	  // copy
+	Object o3 = move(o1); // move
 }
+
+
