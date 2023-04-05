eb8b1a2829868ad172486ac6c498704146d5c4ba
ecourse <smkang@codenuri.co.kr>
2023-04-05 15:20:16 +0900

    Update 5_move추가2.cpp

--- DAY3/5_move추가2.cpp
+++ DAY3/5_move추가2.cpp
@@ -10,12 +10,38 @@ private:
 	std::string name;
 	std::string address;
 public:
+	// 핵심 : 인자가 2개인 setter
 	void set(const std::string& n, const std::string& a)
 	{
 		name = n;
 		address = a;
 	}
+	void set(std::string&& n, std::string&& a)
+	{
+		name = std::move(n);
+		address = std::move(a);
+	}
+	void set(const std::string& n, std::string&& a)
+	{
+		name = n;
+		address = std::move(a);
+	}
+	void set(std::string&& n, const std::string& a)
+	{
+		name = std::move(n);
+		address = a;
+	}
+	// setter인자가 한개 : 2개의 setter 만드세요
+	// 2개이상의 인자를 가진다면 forwarding reference 로 하세요
+	template<typename T1, typename T2>
+	void set(T1&& a1, T2&& a2)
+	{
+		name = std::forward<T1>(a1);
+		address = std::forward<T2>(a2);
+	}
 };
+
+
 
 int main()
 {
