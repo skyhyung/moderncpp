7f06a0d31d7df633b6a13ae2bcf268220b6c4b2a
ecourse <smkang@codenuri.co.kr>
2023-04-05 10:11:31 +0900

    Update 1_forwarding_reference4.cpp

--- DAY3/1_forwarding_reference4.cpp
+++ DAY3/1_forwarding_reference4.cpp
@@ -4,11 +4,37 @@ struct Point
 	Point(int a, int b) : x(a), y(b) {}
 };
 
+
+// 방법 1. call by value
+// => 특징. 복사본을 만들어서 받게 된다.
+// void foo(Point pt) {}
+
+// 방법 2. const lvalue reference
+// => 특징, 복사본이 없다.
+// => 상수성 추가된다. 함수안에서 객체 변경 할수는 없다.
+void foo(const Point& r) {}
+
+//- C++ 98 에서는 위 2개 방법밖에 없다.
+// C++11 이후
+// 방법 3. 2개의 함수 제공
+// => 상수성 추가되지 않고, 복사본도 없다
+void foo(Point& r) {}
+void foo(Point&& r) {}
+
+// 방법 4. forwarding reference
+// => 결국 방법3의 2개 함수를 자동생성하겠다는 의도!!
+template<typename T>
+void foo(T&& r)
+{
+}
+
+
+
 int main()
 {
 	Point pt(0, 0);
 
-	// foo �Լ��� lvalue �� rvalue�� ��� ������ �ʹ�.
+	// foo 함수에 lvalue 와 rvalue를 모두 보내고 싶다.
 	foo(pt);
 	foo(Point(0,0));
 }
