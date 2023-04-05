b5da0dba5bd8a29017f776721f4cc1efa84951a3
ecourse <smkang@codenuri.co.kr>
2023-04-05 13:48:29 +0900

    Update 4_완벽한전달주의사항4.cpp

--- DAY3/4_완벽한전달주의사항4.cpp
+++ DAY3/4_완벽한전달주의사항4.cpp
@@ -4,8 +4,11 @@ int main()
 
 	// 다음중 에러를 "모두" 찾으세요
 
-	auto&  r1 = n;
-	auto&  r2 = 10;
-	auto&& r3 = n;
-	auto&& r4 = 10;
+	auto&  r1 = n;  // ok
+	auto&  r2 = 10; // error
+	auto&& r3 = n;  // ok. int& r3 = n
+	auto&& r4 = 10; // ok. int&& r4 = 10
+
+	auto a = 10;
+	// T a = 인자;
 }
