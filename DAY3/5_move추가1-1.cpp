6adbcf5d71a86f2bd20d0f83dfbdbf6c0d64d1a5
ecourse <smkang@codenuri.co.kr>
2023-04-05 15:11:34 +0900

    Update 5_move추가1-1.cpp

--- DAY3/5_move추가1-1.cpp
+++ DAY3/5_move추가1-1.cpp
@@ -12,10 +12,18 @@ private:
 public:
 	// move 지원 setter 만들기
 	// 방법 1. 2개의 setter.. 
-	void set_name(const std::string& n) { name = n; }
-	void set_name(std::string&& n)      { name = std::move(n); }
+//	void set_name(const std::string& n) { name = n; }
+//	void set_name(std::string&& n)      { name = std::move(n); }
 
-	// 방법 2. 
+	// 방법 2. forwarding reference 를 사용하면 위 2개 자동생성 가능
+	template<typename T>
+	void set_name(T&& n)
+	{
+		// 다음중 맞는 것은 ?
+		name = n;					// 1
+		name = std::move(n);		// 2
+		name = std::forward<T>(n);  // 3
+	}
 };
 
 int main()
