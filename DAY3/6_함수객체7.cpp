f626c841681d0ea59dc89d3068666887d26104de
ecourse <smkang@codenuri.co.kr>
2023-04-05 17:01:02 +0900

    Update 6_함수객체7.cpp

--- DAY3/6_함수객체7.cpp
+++ DAY3/6_함수객체7.cpp
@@ -3,24 +3,33 @@
 
 inline bool cmp1(int a, int b) { return a < b; }
 inline bool cmp2(int a, int b) { return a > b; }
-struct Less   { inline bool operator()(int a, int b) { return a < b; }};
-struct Greater{	inline bool operator()(int a, int b) { return a > b; }};
+struct Less { inline bool operator()(int a, int b) { return a < b; } };
+struct Greater { inline bool operator()(int a, int b) { return a > b; } };
 
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
 
-	// C++ 표준 sort()는 모든 인자가템 플릿 입니다.
+	// C++ 표준 sort()는 모든 인자가 템플릿 입니다.
 
-  // 1. 비교 정책으로 일반함수 사용시
+	// 1. 비교 정책으로 일반함수 사용시
 
-	std::sort(x, x+10, cmp1); 
-	std::sort(x, x+10, cmp2); 
+	// 장점 : 비교 정책을 여러번 교체 해도 sort() 함수는 한개만 생성된다.
+	//        코드메모리 증가하지 않는다.
 
-  // 2. 비교 정책으로 함수객체 사용시
+	// 단점 : 비교정책 함수가 인라인 치환될수 없다. 
+	//        인라인 치환보다는 느리다.
+
+	std::sort(x, x + 10, cmp1); // sort(int*, int*, bool(*)(int,int))인 함수 생성
+	std::sort(x, x + 10, cmp2); // sort(int*, int*, bool(*)(int,int))인 함수 생성
+
+	// 2. 비교 정책으로 함수객체 사용시
 	Less    f1;
 	Greater f2;
-	std::sort(x, x + 10, f1); 
-	std::sort(x, x + 10, f2); 
+	std::sort(x, x + 10, f1); // sort(?) 모양의 함수 생성
+	std::sort(x, x + 10, f2); // sort(?) 모양의 함수 생성
 }
+
+
+
 
