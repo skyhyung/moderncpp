f544ef8ffff824844bf3840d7f160e878a275b22
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-06 22:38:40 +0900

    Update 1_function_argument3.cpp

--- DAY4/1_function_argument3.cpp
+++ DAY4/1_function_argument3.cpp
@@ -15,7 +15,7 @@ void f2(std::string_view   s) {}
 
 int main()
 {
-//	std::string s = "to be or not to be";
+//	std::string s = "to be or not to be"; // new 호출 됨
 
 	// 아래 2개를 비교해 봅시다.
 	// => 아래 2줄은 성능이 거의 유사합니다.
@@ -24,8 +24,8 @@ int main()
 
 	// 그런데.. 아래 2줄을 생각해 보세요
 	f1("to be or not to be");	// string 의 임시객체를 만들게 되고
-								// 메모리를 동적할당해서 문자열 보관 -> static 공간(?)에 있는 string을 소유
-								// new 사용
+					// 메모리를 동적할당해서 문자열 보관 -> static 공간(?)에 있는 string을 소유
+					// new 사용
 	f2("to be or not to be");	// new 사용하지 않음. static 공간(?)에 있는 string 참조.
 }
 
