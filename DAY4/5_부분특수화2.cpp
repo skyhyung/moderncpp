85342c67a91d96c1a5617e3814291f0db04449fd
ecourse <smkang@codenuri.co.kr>
2023-04-06 14:54:15 +0900

    Update 5_부분특수화2.cpp

--- DAY4/5_부분특수화2.cpp
+++ DAY4/5_부분특수화2.cpp
@@ -20,7 +20,7 @@ template<typename T, typename U> struct Object<T*, U>
 // 1. 사용자는 반드시 2개 전달해야 한다. Object<int, int>
 // 
 // 2. 부분 특수화시 템플릿 인자 갯수는 바뀔수 있다 - 아래 참고
-`
+
 // 3. 부분 특수화 모양을 표기할 때는 반드시 인자가 2개 - Object<T,T>
 template<typename T> struct Object<T, T>
 {
