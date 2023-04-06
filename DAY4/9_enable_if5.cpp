3e034029f5f711652e6a67752f25806a419067a0
ecourse <smkang@codenuri.co.kr>
2023-04-06 17:11:00 +0900

    Update 9_enable_if5.cpp

--- DAY4/9_enable_if5.cpp
+++ DAY4/9_enable_if5.cpp
@@ -1,1 +1,26 @@
-// printv บนป็
+#include <iostream>
+#include <type_traits>
+
+template<typename T> 
+std::enable_if_t< std::is_pointer_v<T> > 
+printv(const T& v)
+{
+	std::cout << v << " : " << *v << std::endl;
+}
+
+template<typename T> 
+std::enable_if_t< !std::is_pointer_v<T> >
+printv(const T& v)
+{
+	std::cout << v << std::endl;
+}
+
+
+int main()
+{
+	int n = 10;
+
+	printv(n); 
+	printv(&n); 
+}
+
