22e32e8d3deaec5d49b62ae37ce6c5eace75ee1c
ecourse <smkang@codenuri.co.kr>
2023-04-07 08:47:25 +0900

    Update 1_enable_if.cpp

--- DAY5/1_enable_if.cpp
+++ DAY5/1_enable_if.cpp
@@ -22,8 +22,10 @@ public:
 	// 아래 코드의 에러메세지를 비교 해보세요
 	template<typename T1, typename T2>
 	
-	std::enable_if_t< std::is_same_v<T1, std::string> > 
-		
+//	std::enable_if_t< std::is_same_v<std::remove_cv_t<std::remove_reference_t<T1>>, std::string> > 
+	
+	std::enable_if_t< std::is_convertible_v< T1, std::string> &&
+					  std::is_convertible_v< T2, std::string> >
 	set(T1&& a1, T2&& a2)
 	{
 		name = std::forward<T1>(a1);
@@ -41,11 +43,12 @@ int main()
 {
 	A aaa;
 
-	std::string name = "kim";
+	const std::string name = "kim";
 	std::string addr = "seoul";
 
 	People p;
-//	p.set(name, addr);
-	p.set(aaa,  addr);
+	p.set(name, addr);
+	p.set("lee", addr);
+//	p.set(aaa,  addr);
 
 }
