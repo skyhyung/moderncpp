e7c18e0e1162ed4fb4789d63db41429f964a70d6
ecourse <smkang@codenuri.co.kr>
2023-04-06 10:35:41 +0900

    Update 2_람다표현식6.cpp

--- DAY4/2_람다표현식6.cpp
+++ DAY4/2_람다표현식6.cpp
@@ -5,6 +5,10 @@ int main()
 {
 	// 람다표현식의 정확한 모양
 	auto f1 = [](int a, int b) -> int { return a + b; };
+				// class xxx{}; xxx();
+
+	int n = f1(1, 2);
+			// 이순간 int 반환
 
 	// return 문장이 한개라면 반환타입 생략가능
 	auto f2 = [](int a, int b)  { return a + b; };
