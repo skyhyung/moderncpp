e058fc4cac7d92f380cf7b914d57a9049e5b2f92
ecourse <smkang@codenuri.co.kr>
2023-04-06 09:59:30 +0900

    Update 2_람다표현식4.cpp

--- DAY4/2_람다표현식4.cpp
+++ DAY4/2_람다표현식4.cpp
@@ -1,14 +1,22 @@
-// 5_����ǥ����5
+// 5_람다표현식5
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-// ����ǥ���İ� Ÿ��
+// 람다표현식과 타입
 
 int main()
 {
 	auto f1 = [](int a, int b) { return a + b; };
+			// class xxx{ operator()(int, int) {} }; xxx();			
+
 	auto f2 = [](int a, int b) { return a + b; };
+			// class yyy{ operator()(int, int) {} }; yyy();			
+
+	// 핵심 : 모든 람다 표현식은 다른 타입 입니다.
+	// => 아래 처럼 타입 이름을 출력해 보거나, godbolt 에서도 확인됩니다.
+	std::cout << typeid(f1).name() << std::endl;
+	std::cout << typeid(f2).name() << std::endl;
 
 }
 
