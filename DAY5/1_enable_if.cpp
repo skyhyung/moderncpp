47edede98865abbbd3dbb598e133a6f1c4def916
ecourse <smkang@codenuri.co.kr>
2023-04-07 08:40:54 +0900

    Update 1_enable_if.cpp

--- DAY5/1_enable_if.cpp
+++ DAY5/1_enable_if.cpp
@@ -10,22 +10,42 @@ private:
 	std::string name;
 	std::string address;
 public:
+	/*
+	// 아래 코드의 일때의 에러메세지와
 	template<typename T1, typename T2>
 	void set(T1&& a1, T2&& a2)
 	{
 		name = std::forward<T1>(a1);
 		address = std::forward<T2>(a2);
 	}
+	*/
+	// 아래 코드의 에러메세지를 비교 해보세요
+	template<typename T1, typename T2>
+	
+	std::enable_if_t< std::is_same_v<T1, std::string> > 
+		
+	set(T1&& a1, T2&& a2)
+	{
+		name = std::forward<T1>(a1);
+		address = std::forward<T2>(a2);
+	}
+
 };
+
+
+
+
+class A {};
 
 int main()
 {
+	A aaa;
+
 	std::string name = "kim";
 	std::string addr = "seoul";
 
 	People p;
-	p.set(name, addr);
-	p.set(std::move(name), addr);
-	p.set(name, std::move(addr));
-	p.set(std::move(name), std::move(addr));
+//	p.set(name, addr);
+	p.set(aaa,  addr);
+
 }
