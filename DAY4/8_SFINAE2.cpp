aa83cd022ecf8938fe128eb59f0a03d4e91bcb2a
ecourse <smkang@codenuri.co.kr>
2023-04-06 16:43:55 +0900

    Update 8_SFINAE2.cpp

--- DAY4/8_SFINAE2.cpp
+++ DAY4/8_SFINAE2.cpp
@@ -2,11 +2,18 @@
 
 
 template<typename T>
-int foo(T a)  { std::cout << "T" << std::endl; return 0; }
+typename T::type foo(T a)  { std::cout << "T" << std::endl; return 0; }
 
-void foo(...) { std::cout << "..." << std::endl; }
+
+int foo(...) { std::cout << "..." << std::endl; return 0; }
+
 
 int main()
 {
-	foo(3); 
+	foo(3); // 1. 인자인 3을 보고 "foo(T)"버전 선택
+			// 2. T=int 를 정하고 함수를 생성하다가 실패 하면
+			//    => 에러는 아니고 후보에서 제외된다.
+			//	  => 동일 이름의 다른 함수가 있다면 사용
+
+	// Substitution Failure Is Not An Error => SFINAE
 }
