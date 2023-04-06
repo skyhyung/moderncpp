2037fe3bda344c2d746d3415f8bc43f68ce30f78
ecourse <smkang@codenuri.co.kr>
2023-04-06 09:55:51 +0900

    Update 2_람다표현식3.cpp

--- DAY4/2_람다표현식3.cpp
+++ DAY4/2_람다표현식3.cpp
@@ -23,9 +23,10 @@ int main()
 	int n = f(1, 2); // 1 + 2
 
 	// 다음중 에러는 ?
-	auto        f1 = [](int a, int b) { return a + b; };
-	auto&       f2 = [](int a, int b) { return a + b; };
-	const auto& f3 = [](int a, int b) { return a + b; };
-	auto&&      f4 = [](int a, int b) { return a + b; };
+	auto        f1 = [](int a, int b) { return a + b; }; // ok.. good
+	auto&       f2 = [](int a, int b) { return a + b; }; // error.
+	const auto& f3 = [](int a, int b) { return a + b; }; // ok.
+											// 하지만 f1이 더 좋습니다.
+	auto&&      f4 = [](int a, int b) { return a + b; }; // ok.
 }
 
