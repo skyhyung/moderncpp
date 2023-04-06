acfc83ab80235aff00ce6c43776126d944470c59
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-06 17:16:12 +0900

    Update 9_enable_if5.cpp

--- DAY4/9_enable_if5.cpp
+++ DAY4/9_enable_if5.cpp
@@ -7,7 +7,7 @@ printv(const T& v)
 {
 	std::cout << v << " : " << *v << std::endl;
 }
-
+//반대 되는 enable_if 가 없으면 둘다 타게 된다...? 확인 해보자
 template<typename T> 
 std::enable_if_t< !std::is_pointer_v<T> >
 printv(const T& v)
