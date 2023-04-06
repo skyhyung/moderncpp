ed57e00efe91601e985f1e00b463c61fcbb72ee1
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-06 23:59:55 +0900

    Update 4_람다기타1.cpp

--- DAY4/4_람다기타1.cpp
+++ DAY4/4_람다기타1.cpp
@@ -13,7 +13,7 @@ public:
 		// 핵심 : 람다 표현식이 this 캡쳐를 사용하면 모든 멤버 데이타 접근가능
 		// auto f = [this](int a) { return a + data; }; // 컴파일러가 a + this->data 로 변경
 		// this pointer를 "복사" 한 것임. pointer를 복사해도 안의 값이 주소이기 때문에 똑같인 원본을 바라본다.
-		auto f = [this]() { data = 100; }; // = this->data
+		auto f = [this]() { data = 100; }; // = this(실제 test class)->data를 복사한 this가 가르키고 있다.
 
 		f(); // data = 100 이 실행됩니다. 될까요 ? 
 			 // 원본 수정일까요 ? 복사본일까요 ?
