503278d9522e99fac5b8017ce371e91b567146e6
ecourse <smkang@codenuri.co.kr>
2023-04-06 17:02:32 +0900

    Update 9_enable_if3.cpp

--- DAY4/9_enable_if3.cpp
+++ DAY4/9_enable_if3.cpp
@@ -2,17 +2,25 @@
 #include <type_traits>
 
 
-// std::enable_if<true,  int>::type
-// std::enable_if<false, int>::type
+// std::enable_if<true,  int>::type  => int
+// std::enable_if<false, int>::type  => "::type" 이 없다
+
+// enable_if 를 사용한 리턴타입 만들기 기술
+// 
+// std::enable_if<조건, 반환타입>::type
+// => 조건이 참이면 반환타입 사용
+// => 조건이 거짓이면 사용안함(에러가 아닌 후보에서 제외)
 
 template<typename T> 
 
-std::enable_if< std::is_integral_v<T>, T>::type
+//typename std::enable_if< std::is_integral_v<T>, T>::type
 
+std::enable_if_t< std::is_integral_v<T>, T>
 gcd(T a, T b)
 {
 	return 0;
 }
+
 
 double gcd(double, double)
 {
