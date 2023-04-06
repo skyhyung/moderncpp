70c15de0ecc50aef7188d53c5de5cd9ff2f48eae
ecourse <smkang@codenuri.co.kr>
2023-04-06 16:08:02 +0900

    Update 6_type_traits8.cpp

--- DAY4/6_type_traits8.cpp
+++ DAY4/6_type_traits8.cpp
@@ -5,8 +5,16 @@ int main()
 {
 	int n = 10;
 
-	n + 1 = 10;
-	(n = 10) = 20;
-	++n = 10;
-	n++ = 10;
+	n + 1 = 10; // error. n+1 은 rvalue 
+
+	// decltype(exp) : exp가 lvalue 면 결과는 lvalue 참조 타입
+	//				   exp가 rvalue 면 결과는 값타입
+
+//	if ( std::is_lvalue_reference<decltype(n + 1)>::value )
+	if ( std::is_lvalue_reference_v<decltype(n + 1)>)
+	{
+		std::cout << "lvalue" << std::endl;
+	}
+	else 
+		std::cout << "rvalue" << std::endl;
 }
