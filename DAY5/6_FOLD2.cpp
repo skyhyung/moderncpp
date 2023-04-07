cf44d817d5fa5f145079639f5885d775968f562c
ecourse <smkang@codenuri.co.kr>
2023-04-07 10:22:14 +0900

    Update 6_FOLD2.cpp

--- DAY5/6_FOLD2.cpp
+++ DAY5/6_FOLD2.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> v;
 
 template<typename ... Types>
 void Show(Types ... args)
 {
+	// 초기값  op ... op pack
+	//(std::cout << ... << args);
+	// ((( std::cout << E1) << E2) <<E3)
+
+	// pack 위치에 pack 을 사용하는 패턴이 와도 됩니다.
+	// pack            op  ...
+	// (v.push_back(args) ,   ...);
+	// ( v.push_back(E1), (v.push_back(E2) , ( v.push_back(E3) )))
+
+	// pack 의 패턴				 op ...	
+	((std::cout << args << ", ")  , ...);
 }
 
 int main()
