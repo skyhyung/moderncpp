8d4228913e73ece394a66fb6a117ff9ce6752fac
ecourse <smkang@codenuri.co.kr>
2023-04-07 13:27:07 +0900

    Create tuple.h

---
+++ DAY5/tuple.h
@@ -0,0 +1,19 @@
+template<typename ... Types>
+struct tuple
+{
+	static constexpr int N = 0;
+};
+
+template<typename T, typename ... Types>
+struct tuple<T, Types...> : public tuple<Types...>
+{
+	using base = tuple<Types...>;
+
+	T value;
+
+	tuple() = default;
+	tuple(const T& v, const Types& ... args)
+		: value(v), base(args...) {}
+
+	static constexpr int N = base::N + 1;
+};
