d5bdb5860fa4a290cbeaf6a9791540ce808b7572
ecourse <smkang@codenuri.co.kr>
2023-04-07 14:52:23 +0900

    Update new2.cpp

--- DAY5/new2.cpp
+++ DAY5/new2.cpp
@@ -14,6 +14,28 @@ int main()
 	Point* p1 = new Point(1, 2);
 
 	// 힙에 객체 10개 만들어 보세요.(연속된 메모리에)
+//	Point* p2 = new Point[10]; // error. 디폴트 생성자가 없습니다.
+
+//	Point* p2 = new Point[10]{ {0,0},{0,0};
+
+	// 생성자 호출과 메모리 할당을 분리하면 유연성이 좋아집니다.
+
+	Point* p2 = static_cast<Point*>(operator new(sizeof(Point) * 10));
+
+	// 이미 할당된 10개의 메모리에 객체를 생성 합니다.(생성자 호출을 의미)
+
+	for (int i = 0; i < 10; i++)
+	{
+		new(&p2[i]) Point(0, 0);
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		p2[i].~Point();
+	}
+
+	operator delete(p2);
+
 }
 
 
