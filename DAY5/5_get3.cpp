20bebd711d74afae74951a84234cda40c857cd69
ecourse <smkang@codenuri.co.kr>
2023-04-07 13:41:29 +0900

    Update 5_get3.cpp

--- DAY5/5_get3.cpp
+++ DAY5/5_get3.cpp
@@ -21,9 +21,32 @@ struct tuple<T, Types...> : public tuple<Types...>
 };
 //---------------------------------------------------------
 
+template<int N, typename T> 
+struct tuple_element
+{
+	using type = T;
+};
+
+// N == 0
+template<typename T, typename ... Types>
+struct tuple_element<0, tuple<T, Types...> >
+{
+	using type = T ;
+};
+
+// N != 0
+template<int N, typename T, typename ... Types>
+struct tuple_element<N, tuple<T, Types...> >
+{
+	using type = typename tuple_element<N - 1, tuple<Types...>>::type;
+};
+
+
+
 template<typename T> void foo(T& tp)
 {
-	typename tuple_element<0, T>::type n;
+	// T : tuple<int, double, char>
+	typename tuple_element<0, T>::type n; // int
 
 	std::cout << typeid(n).name() << std::endl;
 }
@@ -31,4 +54,6 @@ template<typename T> void foo(T& tp)
 int main()
 {
 	tuple<int, double, char> t(1, 3.4, 'A');
+
+	foo(t);
 }
