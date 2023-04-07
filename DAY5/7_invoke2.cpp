6f001677d8c6d8e5320e396e0568c22eda7e4001
ecourse <smkang@codenuri.co.kr>
2023-04-07 14:14:33 +0900

    Update 7_invoke2.cpp

--- DAY5/7_invoke2.cpp
+++ DAY5/7_invoke2.cpp
@@ -1,1 +1,32 @@
-// chronometry ����
+#include <iostream>
+#include <functional>
+
+class Object
+{
+public:
+	void func1(int code) {}
+	static void func2(int code) {}
+};
+
+void foo(int a, int b, int& c) { c = 100; }
+
+
+template<typename F, typename ... Types>
+decltype(auto) chronometry(F f, Types&& ... args)
+{
+//	return f(std::forward<Types>(args)...);
+
+	return std::invoke(f, std::forward<Types>(args)...);
+}
+
+int main()
+{
+	int n = 10;
+	chronometry(foo, 1, 2, n);
+
+	Object obj;
+	chronometry(&Object::func1, &obj, 10); // 멤버 함수의 성능측정
+
+	std::cout << n << std::endl;
+}
+
