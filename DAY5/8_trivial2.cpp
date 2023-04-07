d7681ae51293694765ead8c0c8ca9dce210b51d4
ecourse <smkang@codenuri.co.kr>
2023-04-07 15:13:48 +0900

    Update 8_trivial2.cpp

--- DAY5/8_trivial2.cpp
+++ DAY5/8_trivial2.cpp
@@ -6,6 +6,28 @@ struct Point
 	int x, y;
 };
 
+template<typename T> 
+void copy_type(T* dst, T* src, std::size_t sz)
+{
+	if (std::is_trivially_copyable_v<T>)
+	{
+		std::cout << "memcpy 사용" << std::endl;
+		memcpy(dst, src, sizeof(T) * sz);
+	}
+	else
+	{
+		std::cout << "복사 생성자 사용" << std::endl;
+		
+		while (sz--)
+		{
+		//	new (dst) T;		// 디폴트 생성자
+			new (dst) T(*src);	// 복사 생성자
+
+			++dst, ++src;
+		}
+	}
+}
+
 int main()
 {
 	Point buff1[10];
