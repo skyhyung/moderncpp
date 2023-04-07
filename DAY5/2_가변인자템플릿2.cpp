4142b2739ec259b8649448a56c212f7a77c02b46
ecourse <smkang@codenuri.co.kr>
2023-04-07 09:08:03 +0900

    Update 2_가변인자템플릿2.cpp

--- DAY5/2_가변인자템플릿2.cpp
+++ DAY5/2_가변인자템플릿2.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
-int hoo(int a) { return -a; }
-
-void goo(int a, int b, int c) {}
-
 
 template<typename ... Types> void foo(Types ... args)
 {
+	// Types : template parameter pack
+	// args  : function parameter pack
 
+	// pack 안에 있는 요소의 갯수 구하기
+	std::cout << sizeof...(args) << std::endl;
+	std::cout << sizeof...(Types) << std::endl;
 }
 
 int main()
 {
 	foo(1, 3.4, 'A');
-	foo(1, 2, 3);
 }
+
+
