9ded80f82eef1314eac9cffd8e6d8b67c7c9ee1e
ecourse <smkang@codenuri.co.kr>
2023-04-07 11:33:15 +0900

    Update 4_tuple1.cpp

--- DAY5/4_tuple1.cpp
+++ DAY5/4_tuple1.cpp
@@ -1,9 +1,16 @@
-// 8_tuple1 - 245 page
 #include <tuple>
+#include <iostream>
 
 int main()
 {
-	// pair : ¼­·Î ´Ù¸¥ Å¸ÀÔ 2°³¸¦ º¸°ü - C++98
+	// pair : ì„œë¡œ ë‹¤ë¥¸ íƒ€ìž… 2ê°œë¥¼ ë³´ê´€ - C++98
 	std::pair<int, double> p(1, 2.3);
+	
+	std::cout << p.first << std::endl; // 1
+	
+
+	std::pair<int, std::pair<double, char>> p3(10, { 3.4, 'A' });
+
+	std::cout << ? << std::endl; // 3.4
 
 }
