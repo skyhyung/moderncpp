686d14d2282a150fb154fca30c9549a539fb4b55
ecourse <smkang@codenuri.co.kr>
2023-04-07 14:41:41 +0900

    Update new1.cpp

--- DAY5/new1.cpp
+++ DAY5/new1.cpp
@@ -10,6 +10,36 @@ public:
 };
 int main()
 {
-	Point* p1 = new Point(1, 2);
-	delete p1;
+	// new
+	// 1. 메모리 할당
+	// 2. 생성자 호출
+	// Point* p1 = new Point(1, 2);
+
+	// 1. 소멸자 호출
+	// 2. 메모리 해지
+	// delete p1;
+
+	// 1. 메모리만 할당
+	Point* p = static_cast<Point*>(operator new(sizeof(Point)));
+
+	// 2. 이미 할당된 메모리에 생성자 호출
+	new(p) Point(1, 2);
+
+	// 3. 메모리 해지없이 소멸자만 호출
+	p->~Point();
+
+	// 4. 메모리만 해지
+	operator delete(p);
 }
+
+
+
+
+
+
+
+
+
+
+
+
