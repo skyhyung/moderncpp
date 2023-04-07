ea12be3a7a5895191540b00c9a0381e81eae0f40
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-07 21:55:53 +0900

    Update 2_가변인자템플릿1-1.cpp

--- DAY5/2_가변인자템플릿1-1.cpp
+++ DAY5/2_가변인자템플릿1-1.cpp
@@ -1,23 +1,18 @@
 // 2_가변인자템플릿1-1
+#include <iostream>
 
 template<typename T> void f1(T a) {}
-
 template<typename ... Types> void f2(Types ... args) {}
-
 void f3(...) {}  // 함수가 한개 인데. 다양한 인자를 몇개라도 받는다.
 				 // 위 코드는 함수를 생성하는 것
 
 int main()
 {
-	f1(1);      // ok
+	f1(1);          // ok
 	f1(3.4);	// ok
-	f1(1, 3.4); // error
+	//f1(1, 3.4);   // error
 
-	f2(1);      // ok
-	f2(1, 3.4); // Types : int, double
-				// args  : 1, 3.4
-
+	f2(1);          // ok
+	f2(1, 3.4);     // Types : int, double
+			// args  : 1, 3.4
 }
-
-
-
