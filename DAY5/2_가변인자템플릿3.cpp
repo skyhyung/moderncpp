46c6b0121169659b30196af580ce78ef1b52f8fa
skyhyung <75232135+skyhyung@users.noreply.github.com>
2023-04-07 21:58:17 +0900

    Update 2_가변인자템플릿3.cpp

--- DAY5/2_가변인자템플릿3.cpp
+++ DAY5/2_가변인자템플릿3.cpp
@@ -12,14 +12,14 @@ template<typename ... Types> void foo(Types ... args)
 	// Pack Expansion
 	// "pack 을 사용하는 패턴"...   => E1패턴, E2패턴, E3패턴
 
-	goo(args...);  // ok, goo(1, 2, 3)
-	goo(++args...);// ok. goo(++E1, ++E2, ++E3) 
-				   // 실행후에는 args 는 "2,3,4" 입니다.
+	goo(args...);  		// ok, goo(1, 2, 3)
+	goo(++args...);		// ok. goo(++E1, ++E2, ++E3) 
+				// 실행후에는 args 는 "2,3,4" 입니다.
 
 //	goo(hoo(args...));	// goo(hoo(E1, E2, E3)) 인데..
-						// hoo 는 인자가 한개이므로 error
+				// hoo 는 인자가 한개이므로 error
 
-	goo(hoo(args)...);  // goo( hoo(E1), hoo(E2), hoo(E3)) 
+	goo(hoo(args)...);  	// goo( hoo(E1), hoo(E2), hoo(E3)) 
 }
 
 
